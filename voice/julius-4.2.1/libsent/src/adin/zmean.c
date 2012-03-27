a3e8b0383a3b3c8f75fb2c694f493af27d655a1b
unknown <BobTheElephant@Elephant.(none)>
2012-03-26 21:30:05 -0400

    Modified averaging filter to be continuous.
    Can be activated with -zmean option in jconf file.
    
    Signed-off-by: Zach Gauci <zgauci@gmail.com>
    Signed-off-by: Alex Waterman <imNotListening@gmail.com>

--- voice/julius-4.2.1/libsent/src/adin/zmean.c
+++ voice/julius-4.2.1/libsent/src/adin/zmean.c
@@ -40,8 +40,26 @@
 
 #include <sent/adin.h>
 
+//#define DBG 1
+#ifdef DBG
+static int num_its = 0;
+#endif
+
 static int zlen = 0;		///< Current recorded length for DC offset estimation
 static float zmean = 0.0;	///< Current mean
+
+///< history of prior samples for dynamic level triggering
+static SP16 *history;// = (unsigned int *)mymalloc(HISTORY_LEN * sizeof(SP16));
+static int history_index;    ///< current location in the history array
+static float history_average;
+
+static int zmean_init = 0;
+
+static float max_val_avg;
+static int *max_vals;
+static int max_val_index = 0;
+
+#define NUM_MAX_VALS 25
 
 /** 
  * Reset status.
@@ -50,8 +68,27 @@ static float zmean = 0.0;	///< Current mean
 void
 zmean_reset()
 {
+  int i = 0;
   zlen = 0;
   zmean = 0.0;
+
+  if(zmean_init == 0) {
+    jlog("initing zmean stuff");
+    history = (SP16 *)mymalloc(ZMEANSAMPLES * sizeof(SP16));
+    for (i = 0;i < ZMEANSAMPLES; i++) {
+      history[i] = 0;
+    }
+    history_index = 0;
+    history_average = 0;
+
+    //scaling params
+    max_vals = (int *)mymalloc(NUM_MAX_VALS * sizeof(int));
+    for (i = 0;i < NUM_MAX_VALS; i++) {
+      max_vals[i] = 0;
+    }
+    max_val_avg = 0.0;
+  }
+    zmean_init++;
 }
 
 /** 
@@ -69,25 +106,63 @@ zmean_reset()
 void
 sub_zmean(SP16 *speech, int samplenum)
 {
-  int i;
-  float d, sum;
+    int i, max_value = 0;
+    int tmp = 0;
+    float d;
 
-  if (zlen < ZMEANSAMPLES) {
-    /* update zmean */
-    sum = zmean * zlen;
-    for (i=0;i<samplenum;i++) {
-      sum += speech[i];
+    for ( i = 0; i < samplenum; i++) {
+        /* adjust running average for new value and removal of first value */
+        history_average -= history[history_index] / (float) ZMEANSAMPLES;
+        history_average += abs(speech[i]) / (float) ZMEANSAMPLES ;
+
+        /* update history */
+        history[history_index++] = abs(speech[i]);
+        if( history_index == ZMEANSAMPLES ) history_index = 0;
     }
-    zlen += samplenum;
-    zmean = sum / (float)zlen;
-  }
-  for (i=0;i<samplenum;i++) {
-    d = (float)speech[i] - zmean;
-    /* clip overflow */
-    if (d < -32768.0) d = -32768.0;
-    if (d > 32767.0) d = 32767.0;
-    /* round to SP16 (normally short) */
-    if (d > 0) speech[i] = (SP16)(d + 0.5);
-    else speech[i] = (SP16)(d - 0.5);
-  }
+
+    /* max value */
+    for ( i = 0; i < ZMEANSAMPLES; i++) {
+        if(history[i] > max_value) max_value = history[i];
+    }
+    //calculate max values
+    max_val_avg -= max_vals[max_val_index] / (float) NUM_MAX_VALS;
+    max_val_avg += max_value / (float) NUM_MAX_VALS;
+    max_vals[max_val_index++] = max_value;
+    if (max_val_index == NUM_MAX_VALS) max_val_index = 0;
+    
+    
+
+    for (i=0;i<samplenum;i++) {
+        d = (float)speech[i] - max_val_avg;
+//        d = (float)speech[i] - history_average * SCALE_VAL;
+        /* clip overflow */
+        if (d < -32768.0) d = -32768.0;
+        if (d > 32767.0) d = 32767.0;
+        /* round to SP16 (normally short) */
+        if (d > 0) speech[i] = (SP16)(d + 0.5);
+        else speech[i] = (SP16)(d - 0.5);
+    }
+
+#ifdef DBG
+    if(DBG == 2) {
+        jlog("{ %d", history[0]);
+        for(i=1; i<ZMEANSAMPLES; i++) {
+            jlog(", %hd", history[i]);
+        }
+        jlog(" }\t");
+        jlog("avg = %f\n", history_average);
+    } 
+    else if(DBG == 1) {
+        num_its++;
+        if( num_its % 10 == 1){
+            jlog("avg = %f\t", history_average);
+            jlog("max_in = %d\t", max_value);
+            for(i=0;i<ZMEANSAMPLES; i++){
+                if(speech[i] > tmp) tmp = speech[i];
+            }
+            jlog("max_avg = %f\t", max_val_avg);
+            jlog("max_out = %hd\n", tmp);
+        }
+    }
+#endif
 }
